c504cb8153cbdd74ab43104a4c3fe1cc241a22eb
Eli Smukler <eli@smukler.com>
2015-03-31 16:11:06 -0400

    add smooth transitions to info panes

--- eli.js
+++ eli.js
@@ -2,8 +2,22 @@ $(function(){
   $(".nav-list li").click(function(event) {
     var contentId = $(event.currentTarget).attr("data-id");
     var $newContent = $("[data-link-id='" + contentId + "']");
-    var $oldContent = $(".active")
-    $oldContent.toggleClass("active hidden")
-    $newContent.toggleClass("active hidden");
+    var oldId = $(".active").attr("data-link-id");
+    var $oldContent = $("[data-link-id='" + oldId + "']")
+    $oldContent.toggleClass("active transitioning");
+
+
+
+    $oldContent.one("transitionend", function(event) {
+      $oldContent.toggleClass("transitioning hidden")
+
+      $newContent.toggleClass("hidden active transitioning")
+
+      setTimeout(function() {
+        $newContent.removeClass("transitioning");
+      }, 0);
+    })
+
+
   })
 })
