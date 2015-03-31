63672bda4b88cece29876aa9d8dbeeb72062c383
Eli Smukler <eli@smukler.com>
2015-03-31 16:35:23 -0400

    pane doesn't change to itself

--- eli.js
+++ eli.js
@@ -2,22 +2,22 @@ $(function(){
   $(".nav-list li").click(function(event) {
     var contentId = $(event.currentTarget).attr("data-id");
     var $newContent = $("[data-link-id='" + contentId + "']");
-    var oldId = $(".active").attr("data-link-id");
-    var $oldContent = $("[data-link-id='" + oldId + "']")
-    $oldContent.toggleClass("active transitioning");
 
+    if (!$newContent.hasClass("active")) {
+      var $oldContent = $(".active")
 
+      $oldContent.toggleClass("active transitioning");
 
-    $oldContent.one("transitionend", function(event) {
-      $oldContent.toggleClass("transitioning hidden")
+      $oldContent.one("transitionend", function(event) {
+        $oldContent.toggleClass("transitioning hidden")
 
-      $newContent.toggleClass("hidden active transitioning")
+        $newContent.toggleClass("hidden active transitioning")
 
-      setTimeout(function() {
-        $newContent.removeClass("transitioning");
-      }, 0);
-    })
-
+        setTimeout(function() {
+          $newContent.removeClass("transitioning");
+        }, 0);
+      })
+    }
 
   })
 })
